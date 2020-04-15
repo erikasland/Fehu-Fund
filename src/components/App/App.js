0184ecd58f68a718f914232a7e4b5b9e4c82c203
Erik Asland <erikasland87@gmail.com>
2020-04-15 16:37:49 -0500

    initial-commit

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Navigation from'../Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <section>
+        <div className="intro-para-line-wrapper">
+          
+        </div>
+        <div className="continue-arrow">
+          <div>
+            <img />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navigation = () => (
+    <nav>
+        <div>
+          <h3>Home</h3>
+        </div>
+        <div>
+          <h3>About</h3>
+        </div>
+        <div>
+          <h3>Team</h3>
+        </div>
+        <div>
+          <h3>Resources</h3>
+        </div>
+        <div>
+          <h3>Assets</h3>
+        </div>
+        <div>
+          <h3>Contact</h3>
+        </div>
+      </nav>
+)
+
+export default Navigation;
